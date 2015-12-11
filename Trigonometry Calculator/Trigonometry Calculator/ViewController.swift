d03700af90fca6ee6e4fe146c9b601ac98a31e77
Nick Jones <njones@rsgc.on.ca>
2015-12-11 10:51:24 -0500

    Basic Layout

---
+++ Trigonometry Calculator/Trigonometry Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trigonometry Calculator
+//
+//  Created by Student on 2015-12-11.
+//  Copyright © 2015 Nick Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
