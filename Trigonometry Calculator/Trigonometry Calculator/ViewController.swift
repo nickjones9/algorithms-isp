5950f332b3d6ae5d5136644aa35fd5d8eb32131e
Nick Jones <njones@rsgc.on.ca>
2016-01-08 21:33:42 -0500

    Basic Interface Build

--- Trigonometry Calculator/Trigonometry Calculator/ViewController.swift
+++ Trigonometry Calculator/Trigonometry Calculator/ViewController.swift
@@ -1,4 +1,4 @@
-//
+//  Nick Jones
 //  ViewController.swift
 //  Trigonometry Calculator
 //
@@ -19,7 +19,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
+    
+//    @IBOutlet weak var AngleAText: UITextField!     // Gather info from the text field
+//    @IBOutlet weak var AngleBText: UITextField!
+//    @IBOutlet weak var AngleCText: UITextField!
+//    @IBOutlet weak var SideaText: UITextField!
+//    @IBOutlet weak var SidebText: UITextField!
+//    @IBOutlet weak var SidecText: UITextField!
+    
+    var AngleA: Float = 0.00                        // Naming variables for the angles, = 0
+    var AngleB: Float = 0.00
+    var AngleC: Float = 90.00
+    var Sidea: Float = 0.00                         // Naming variables for the sides, = 0
+    var Sideb: Float = 0.00
+    var Sidec: Float = 0.00
+    
+  }
 
