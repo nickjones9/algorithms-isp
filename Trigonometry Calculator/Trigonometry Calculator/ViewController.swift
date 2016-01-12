a3161de6268592fae0543e6a472c73dba875a8fc
Nick Jones <njones@rsgc.on.ca>
2016-01-12 11:53:27 -0500

    Restore From Backup

---
+++ Trigonometry Calculator/Trigonometry Calculator/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Trigonometry Calculator
+//
+//  Created by Student on 2015-12-11.
+//  Copyright © 2015 Nick Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var AngleATextField: UITextField!
+    @IBOutlet weak var AngleBTextField: UITextField!
+
+    @IBOutlet weak var SideaTextField: UITextField!
+    @IBOutlet weak var SidebTextField: UITextField!
+    @IBOutlet weak var SidecTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func CalculateButton(sender: AnyObject) {
+        
+        print ("Calculate Botton Pressed")
+        
+    }
+hellloooooo
+}
+
