fcf3436885e1131871f7c23083c723f9ac051034
Nick Jones <njones@rsgc.on.ca>
2016-01-13 08:31:15 -0500

    Comments

--- Trigonometry Calculator/Trigonometry Calculator/ViewController.swift
+++ Trigonometry Calculator/Trigonometry Calculator/ViewController.swift
@@ -1,8 +1,8 @@
-//
+//  Nick Jonwa
 //  ViewController.swift
 //  Trigonometry Calculator
 //
-//  Created by Student on 2015-12-11.
+//  Created by Nick Jones on 2015-12-11.
 //  Copyright © 2015 Nick Jones. All rights reserved.
 //
 
@@ -10,35 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var AngleATextField: UITextField!
+    @IBOutlet weak var AngleATextField: UITextField!               // Connecting storyboard Angle textfields to code
     @IBOutlet weak var AngleBTextField: UITextField!
 
-    @IBOutlet weak var SideaTextField: UITextField!
+    @IBOutlet weak var SideaTextField: UITextField!                // Connecting storyboard Side textfields to code
     @IBOutlet weak var SidebTextField: UITextField!
     @IBOutlet weak var SidecTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func CalculateButton(sender: AnyObject) {
+    @IBAction func CalculateButton(sender: AnyObject) {            // Connecting Button to code. When it is pressed...
         
-        print ("Calculate Botton Pressed")
+        print ("Calculate Botton Pressed")                         // Print message into computer
         
-        var A = AngleATextField.text
+        var A = AngleATextField.text                               // Naming the Angles and giving them inserted values
         var B = AngleBTextField.text
         var C = 90
-        var a = SideaTextField.text
+        var a = SideaTextField.text                                // Naming the Sides and giving them inserted values
         var b = SidebTextField.text
         var c = SidecTextField.text
         
-        print (A)
+        print (A)                                                  // Printing values in computer
         print (B)
         print (C)
         print (a)
