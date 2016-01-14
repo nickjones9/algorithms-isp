c65a1350b6c48ea5bcff57bedb507cac3110036f
Nick Jones <njones@rsgc.on.ca>
2016-01-14 10:21:34 -0500

    TextField Values as Float

--- Trigonometry Calculator/Trigonometry Calculator/ViewController.swift
+++ Trigonometry Calculator/Trigonometry Calculator/ViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+extension String {                                                 // Creating an extention to let textfield be named a float value
+    var floatValue: Float {
+        return (self as NSString).floatValue
+    }
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var AngleATextField: UITextField!               // Connecting storyboard Angle textfields to code
@@ -29,23 +35,50 @@ class ViewController: UIViewController {
         
         print ("Calculate Botton Pressed")                         // Print message into computer
         
-        var A = AngleATextField.text                               // Naming the Angles and giving them inserted values
-        var B = AngleBTextField.text
+        var A = AngleATextField.text!.floatValue                   // Naming the Angles and giving them inserted values
+        var B = AngleBTextField.text!.floatValue
         var C = 90
-        var a = SideaTextField.text                                // Naming the Sides and giving them inserted values
-        var b = SidebTextField.text
-        var c = SidecTextField.text
+        var a = SideaTextField.text!.floatValue                    // Naming the Sides and giving them inserted values
+        var b = SidebTextField.text!.floatValue
+        var c = SidecTextField.text!.floatValue
         
-        print (A)                                                  // Printing values in computer
-        print (B)
-        print (C)
-        print (a)
-        print (b)
-        print (c)
+//        print (A)                                                  // Printing values in computer
+//        print (B)
+//        print (C)
+//        print (a)
+//        print (b)
+//        print (c)
         
-//        if (A > 0){
-//            print("A has value")
-//        }
+        
+        if (A > 0){
+            print("A has been declaired")
+        } else {
+            print ("A was not declaired")
+        }
+        
+        if (B > 0){
+            print("B has been declaired")
+        } else {
+            print ("B was not declaired")
+        }
+        
+        if (a > 0){
+            print("a has been declaired")
+        } else {
+            print ("a was not declaired")
+        }
+        
+        if (b > 0){
+            print("b has been declaired")
+        } else {
+            print ("b was not declaired")
+        }
+        if (c > 0){
+            print("c has been declaired")
+        } else {
+            print ("c was not declaired")
+        }
+        
     }
 }
 
